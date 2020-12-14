e5f0f30f19c607f6619af97c5f825ba59159d8ba
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 19:59:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.count == 2
-    return "#{array[0]} and #{array[1]}"
+    #return "#{array[0]} and #{array[1]}"
     return array.join(' and ')
   elsif 2 < array.length
     array[-1].insert(0, "and ")
